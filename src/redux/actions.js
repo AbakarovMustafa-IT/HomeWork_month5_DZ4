bdf7c7fa1beed0b30024f8ad71c017bbfdd02039
AbakarovMustafa-IT <webit2802@gmail.com>
2023-11-25 08:52:41 +0600

    HomeWork_moth5_DZ4

---
+++ src/redux/actions.js
@@ -0,0 +1,14 @@
+export function addUserAction(user) {
+  return async function (dispatch) {
+    const options = {
+      method: "POST",
+      header: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    }
+    const response = await fetch("https://jsonplaceholder.typicode.com", options)
+
+
+  }
+}
